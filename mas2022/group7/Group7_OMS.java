b072e66863b35e8b4b92ae73f349301b8b7fd6d5
Gustavo Luchetti <gusta.luchetti@gmail.com>
2022-02-17 22:41:56 +0100

    feat: stupid template for negotiation agent assigment

---
+++ mas2022/group7/Group7_OMS.java
@@ -0,0 +1,17 @@
+import genius.core.actions.Action;
+import genius.core.parties.AbstractNegotiationParty;
+
+import java.util.List;
+
+public class Group7 extends AbstractNegotiationParty {
+
+    @Override
+    public Action chooseAction(List<Class<? extends Action>> possibleActions) {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
