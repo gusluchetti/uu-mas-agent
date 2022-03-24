82144f608511d7da51834c0765ce89b816a0c1d5
Gustavo Luchetti <gusta.luchetti@gmail.com>
2022-03-24 15:47:27 +0100

    feat: start opponent model

--- src/mas2022/group7/Group7_OM.java
+++ src/mas2022/group7/Group7_OM.java
@@ -3,7 +3,7 @@ package bilateralexamples.mas2022.group7;
 import genius.core.Bid;
 import genius.core.boaframework.OpponentModel;
 
-public class Group7_OM extends OpponentModel{
+public class Group7_OM extends OpponentModel {
     @Override
     protected void updateModel(Bid bid, double time) {
 
