f7fd6adcf7a11372ed674785d67b7bae058319fe
Gustavo Luchetti <gusta.luchetti@gmail.com>
2022-03-24 23:58:45 +0100

    updating files

---
+++ genius-9.1.13/bilateralexamples/Group7_OM.java
@@ -0,0 +1,11 @@
+package bilateralexamples;
+
+import genius.core.Bid;
+import genius.core.boaframework.OpponentModel;
+
+public class Group7_OM extends OpponentModel {
+    @Override
+    protected void updateModel(Bid bid, double time) {
+
+    }
+}
