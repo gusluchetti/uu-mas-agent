aa550ef29213c67b6993e9210358e731e2b20fe9
Gustavo Luchetti <gusta.luchetti@gmail.com>
2022-04-05 20:56:22 +0200

    update AS

--- genius-9.1.13/src/mas2022/group7/Group7_AS.java
+++ genius-9.1.13/src/mas2022/group7/Group7_AS.java
@@ -13,42 +13,40 @@ public class Group7_AS extends AcceptanceStrategy {
     public Bid lastOffer;
     public AbstractUtilitySpace utilitySpace;
     public List<Bid> acceptableBids = new ArrayList();
-    public double accept_util;
-    List<BidDetails> nBestBids;
+    public double accep_util;
+    List<BidDetails> nLastBids;
     double tot_util;
     List<BidDetails> opponent_bids;
     List<Double> list_with_util = new ArrayList();
-
     double sum;
     double sum_index;
     double sum_denominator;
     double sum_numerator;
     double slope;
     double intercept;
-
-    final String ABS_TRESH = "abs_tresh";
-    final String DECLINE_START = "time to start declining";
-    final String RES_VALUE = "first res value";
-    final String OPP_BIDS_AMOUNT = "amount of opponent bids used";
+    double sumofallbidsutil;
+    List<BidDetails> all_biddetails;
+    boolean isavgcalc;
+    double avgofallutil;
 
     public Set<BOAparameter> getParameterSpec() {
-        Set<BOAparameter> parameters = new HashSet();
-        // From 0.6 time the decline starts happening from 0.9
-        parameters.add(new BOAparameter(this.ABS_TRESH, 0.8D, "absolute lowest utility that is acceptable"));
-        parameters.add(new BOAparameter(this.DECLINE_START, 0.6D, "time to start declining"));
-        parameters.add(new BOAparameter(this.RES_VALUE, 0.9D, "first reservation value"));
-        parameters.add(new BOAparameter(this.OPP_BIDS_AMOUNT, 5D, "amount of opponent bids used"));
-        return parameters;}
+        HashSet parameters1 = new HashSet();
+        // From 0.6 time the decline starts happening from 0.95
+        parameters1.add(new BOAparameter("time to start declining", 0.6D, "time to start declining"));
+        parameters1.add(new BOAparameter("first utility value", 0.95D, "first utility value"));
+        parameters1.add(new BOAparameter("amount of opponent bids used", 5D, "amount of opponent bids used"));
+        return parameters1;}
 
 
     @Override
     public Actions determineAcceptability() {
+        sumofallbidsutil = 0;
         sum = 0;
         sum_index = 0;
         opponent_bids = this.negotiationSession.getOpponentBidHistory().getHistory();
         Map<String, Double> par = this.getParameters();
         //make the utility prediction of the opponents bids after certain time is passed so that the line becomes more reliable.
-        if (this.negotiationSession.getTime() > par.get(this.DECLINE_START)){
+        if (this.negotiationSession.getTime() > par.get("time to start declining")){
             for(int i = 0; i < this.opponent_bids.size(); i++){
                 double util = this.opponent_bids.get(i).getMyUndiscountedUtil();
                 list_with_util.add(util);
@@ -58,7 +56,7 @@ public class Group7_AS extends AcceptanceStrategy {
             //least squares regression to find the best fit of a line through the utilities of the opponent.
             //avg of y coordinates (avg_utility);
             double avg_y = sum / this.opponent_bids.size();
-            //avg of x_coordinates is the average of the indexes
+            //avg of x_coordinates is the average of the indexs
             double avg_x = sum_index / this.opponent_bids.size();
             //slope = sum((x-xa)(y-ya))/ sum((x-xa)(x-xa))
             sum_numerator = 0;
@@ -80,60 +78,83 @@ public class Group7_AS extends AcceptanceStrategy {
             //line becomes y = slope * x + intercept
         }
         /*Part 1 where the acceptability starts as a line and after a certain time start declining parabolically.
-        (parameters: time to start declining,first res value) */
-        BidIterator allBids = new BidIterator(this.negotiationSession.getDomain());
+        (parameters: time to start declining,first util value) */
+        BidIterator allbids = new BidIterator(this.negotiationSession.getDomain());
 
-        if (par.containsKey(this.RES_VALUE) && this.negotiationSession.getTime() < par.get(this.DECLINE_START)){
-            accept_util = par.get(this.RES_VALUE);
+        if (this.negotiationSession.getTime() < par.get("time to start declining")){
+            accep_util = par.get("first utility value");
+            //the average utility is only needed after the time it start declining. so whether it is calculated is set to false;
+            isavgcalc = Boolean.FALSE;
+
         }
         else {
             BidDetails opponent_bids_best = this.negotiationSession.getOpponentBidHistory().getBestBidDetails();
             // the opponent best bid utility
             double util_best_opp_bid = opponent_bids_best.getMyUndiscountedUtil();
             //approximation of total amount of bids of opponent
-            double amount_of_bids = this.opponent_bids.size()/this.negotiationSession.getTime();
+            double amount_ofbids = this.opponent_bids.size()/this.negotiationSession.getTime();
 
             /*the predicted utility of the opponents last bid;
             use line you created in the previous section
             line becomes y = slope * x + intercept*/
-            double pred = slope * amount_of_bids + intercept;
+            double pred = slope * amount_ofbids + intercept;
+            //@@@@
+            all_biddetails = this.negotiationSession.getOutcomeSpace().getAllOutcomes();
+            //find avg of all util once by using boolean to whether it was calculated;
+            if (!isavgcalc) {
+                for (int i = 0; i < all_biddetails.size(); i++) {
+                    sumofallbidsutil += all_biddetails.get(i).getMyUndiscountedUtil();
+                }
+                avgofallutil = sumofallbidsutil / all_biddetails.size();
+                //now it is calculated
+                isavgcalc = Boolean.TRUE;
+            }
 
             // if the prediction of the last point is higher than the utility of the opponents highest bid then use it as an endpoint.
             if (pred > util_best_opp_bid){
                 /*pred = accep_util - (timeleft) * decline ^ 2
                  endpoint means the point of lowest acceptability utility at the end of the time*/
                 double endpoint = pred;
-                //failsafe since you dont want it to go lower than 0.6
-                if (endpoint < 0.6){
-                    endpoint = 0.6;
+                //failsafe since you dont want it to go lower than the average of all bids and this depends a lot on the domain;
+                if (endpoint < avgofallutil){
+                    endpoint = avgofallutil;
+                }
+                //failsafe if the endpoint is higher than acceptutil then it should go up but instead we will let it stay the same.-
+                if (endpoint > accep_util){
+                    endpoint = accep_util ;
                 }
                 // decline = sqrt((pred - accep_util) / - (timeleft)))
                 // this decline rate will take care that the accept util will finish at the endpoint at the end of time.
-                double rateofdecline = Math.sqrt((endpoint - accept_util) / - (1- this.negotiationSession.getTime()));
+                double rateofdecline = Math.sqrt((endpoint - accep_util) / - (1- this.negotiationSession.getTime()));
 
                 // to find the average timestep between opponent bids
                 double timestep = this.negotiationSession.getTime() / this.opponent_bids.size();
-                accept_util = accept_util - (timestep * Math.pow(rateofdecline, 2));
+                accep_util = accep_util - (timestep * Math.pow(rateofdecline, 2));
             }
             //else use the utility of the util_best_opp_bid
             else{
                 double endpoint = util_best_opp_bid;
-                //failsafe since you dont want it to go lower than 0.6
-                if (endpoint < 0.6){
-                    endpoint = 0.6;
+                //failsafe since you dont want it to go lower than the average of all bids util
+                if (endpoint < avgofallutil){
+                    endpoint = avgofallutil;
+                }
+                //failsafe if the endpoint is higher than acceptutil then it should go up but instead we will let it stay the same.
+                if (endpoint > accep_util){
+                    endpoint = accep_util ;
                 }
                 // this decline rate will take care that the accept util will finish at the endpoint at the end of time.
-                double rateofdecline = Math.sqrt((endpoint - accept_util) / - (1- this.negotiationSession.getTime()));
+                double rateofdecline = Math.sqrt((endpoint - accep_util) / - (1- this.negotiationSession.getTime()));
 
                 // to find the average timestep between opponent bids
                 double timestep = this.negotiationSession.getTime() / this.opponent_bids.size();
-                accept_util = accept_util - (timestep * Math.pow(rateofdecline, 2));
+                accep_util = accep_util - (timestep * Math.pow(rateofdecline, 2));
+
             }
         }
 
-        while (allBids.hasNext()) {
-            Bid next_bid = allBids.next();
-            if (this.negotiationSession.getUtilitySpace().getUtility(next_bid) > accept_util) {
+        while (allbids.hasNext()) {
+            Bid next_bid = allbids.next();
+            if (this.negotiationSession.getUtilitySpace().getUtility(next_bid) > accep_util) {
                 acceptableBids.add(next_bid);
             }
         }
@@ -144,6 +165,10 @@ public class Group7_AS extends AcceptanceStrategy {
             return Actions.Accept;
         }
 
+        if (lastopponentbid.getMyUndiscountedUtil() >= accep_util){
+            return Actions.Accept;
+
+        }
         // part 2, if the opponents bid has a higher or equal utility than our next bid then accept
         if (this.offeringStrategy.getNextBid().getMyUndiscountedUtil() <= lastopponentbid.getMyUndiscountedUtil()){
             return Actions.Accept;
@@ -153,19 +178,22 @@ public class Group7_AS extends AcceptanceStrategy {
          and thus if it above a certain threshold then we accept.  (parameters: size of history that you want to include)*/
         int size = this.negotiationSession.getOpponentBidHistory().size();
 
-        if (this.negotiationSession.getOpponentBidHistory().getHistory().size() < par.get(this.OPP_BIDS_AMOUNT).intValue()){
-            nBestBids = this.negotiationSession.getOpponentBidHistory().getHistory().subList(0, size);
+        if (this.negotiationSession.getOpponentBidHistory().getHistory().size() < par.get("amount of opponent bids used").intValue()){
+            nLastBids = this.negotiationSession.getOpponentBidHistory().getHistory().subList(0, size);
         }
-        else{nBestBids = this.negotiationSession.getOpponentBidHistory().getHistory().subList(size - par.get(this.OPP_BIDS_AMOUNT).intValue(), size);}
+        else{nLastBids = this.negotiationSession.getOpponentBidHistory().getHistory().subList(size - par.get("amount of opponent bids used").intValue(), size);}
 
         tot_util = 0;
-        for (int i = 0; i < nBestBids.size();i++){
-            tot_util += nBestBids.get(i).getMyUndiscountedUtil();
+        for (int i = 0; i < nLastBids.size();i++){
+            tot_util += nLastBids.get(i).getMyUndiscountedUtil();
 
         }
-        double avg_util = tot_util / nBestBids.size();
-        //this is used to find bids that happen only once in the negotation session and thus are one of the best that can be accepted
-        if (avg_util * 2 < lastopponentbid.getMyUndiscountedUtil() && lastopponentbid.getMyUndiscountedUtil()> par.get(this.ABS_TRESH) && nBestBids.size() >= par.get(this.OPP_BIDS_AMOUNT).intValue() ){
+        double avg_util = tot_util / nLastBids.size();
+        /*this is used to find bids that happen only once in the negotation session and thus are one of the best that can be accepted
+        it could be the case that the opponents makes bids that are a little below the acceptance utility and are an outlier compared
+        to the last previous bids thus it could be beneficial to accept the offer. (it perhaps is an onetime-offer)
+         */
+        if (avg_util * 2 < lastopponentbid.getMyUndiscountedUtil() && lastopponentbid.getMyUndiscountedUtil()> (accep_util * 0.9)  && nLastBids.size() >= par.get("amount of opponent bids used").intValue() ){
             return Actions.Accept;
         }
         //If neither of these things are satisfied then reject
@@ -174,6 +202,6 @@ public class Group7_AS extends AcceptanceStrategy {
 
 
     public String getName() {
-        return "Agent 007 - Acceptance Strategy";
+        return "acceptance strategy agent 007 -1";
     }
 }
